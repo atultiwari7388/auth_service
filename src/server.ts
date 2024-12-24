f615c2fe3a626f95f622bedc11f68615911ef29b
Atul Tiwari <tiwariatul9526@gmail.com>
2024-12-24 17:51:41 +0530

    10. add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import app from './app'
 import { Config } from './config'
 
-console.log(Config.PORT)
+const startServer = () => {
+    const PORT = Config.PORT || 3000
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`)
+        })
+    } catch (err) {
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+startServer()
